9814158ef6d4a84746f3ee0113158e15926888df
Leonardo Viana <codezeraoficial@gmail.com>
2020-12-13 13:35:52 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',  
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
