9f37a7e295bcc8fd49810bb4065e27e4de89f92f
Leonardo Viana <codezeraoficial@gmail.com>
2021-01-05 08:26:38 -0300

    feat: ensure Login starts with inputs with required sate

--- src/presentation/components/input/index.tsx
+++ src/presentation/components/input/index.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { FcCancel } from 'react-icons/fc'
 import Styles from './styles.scss'
+import Context from '@/presentation/contexts/form/form-context'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
+  const { errorState } = useContext(Context)
+  const error = errorState[props.name]
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
+  }
+
+  const getStatus = (): any => {
+    return <FcCancel/>
+  }
+
+  const getTitle = (): string => {
+    return error
   }
   return (
     <div className={Styles.inputWrap}>
       <input {...props} readOnly onFocus={enableInput}/>
-      <span className={Styles.status}><FcCancel/></span>
+      <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
     </div>
   )
 }
